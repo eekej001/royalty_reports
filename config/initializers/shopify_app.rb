82d35fa7e43c67961f45ca8067645f1bfcaefea0
Ben <eekej001@gmail.com>
2017-09-13 14:10:51 -0700

    updated config info

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
 
-  config.api_key = "55e4b2fbc12da207312f26d934e67ea6"
-  config.secret = "9d2d705742d66e7cf745349171ecb72c"
+  config.api_key = "5b6c8beffd764fc95d6bcde8459c0b2a"
+  config.secret = "a87f1122752655beffb8aa3da1a87e9d"
   config.scope = "read_orders, read_products, write_products"
   config.embedded_app = true
 end
