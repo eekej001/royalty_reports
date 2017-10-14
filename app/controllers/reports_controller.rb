e54bf5127746be8c7a540a6a567532e02ae5c2c9
Ben <eekej001@gmail.com>
2017-10-13 23:28:29 -0700

    added sass

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,6 +4,19 @@ class ReportsController < ShopifyApp::AuthenticatedController
     @artists = Artist.all
  end
 
-
+ def report
+ 	id = params[:id]
+ 	start_date = params[:start_date]
+ 	end_date = params[:end_date]
+    flash[:notice] = "The artist id is #{id}.  The start date is #{start_date}.  The end date is #{end_date}."
+    redirect_to(:action => 'index')
 
 end
+
+
+private
+
+    def form_params
+      params.require(:form).permit(:id, :start_date, :end_date)
+    end
+
