364a39458b6ebe89369a35a16db713da939c84ec
Ben <eekej001@gmail.com>
2017-11-02 16:07:29 -0700

    added delayed jobs for heroku

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,3 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'rubygems'
-require 'yaml'
-YAML::ENGINE.yamler = 'syck'
