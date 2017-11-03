84ece6cb5ab5b935bb72a9c9da88351cd114a2f3
Ben <eekej001@gmail.com>
2017-11-03 14:15:19 -0700

    added delayed jobs for heroku

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -5,6 +5,6 @@ class Shop < ActiveRecord::Base
   def self.shopify_api_path
     #"https://#{Rails.configuration.api_key}:#{self.shopify_token}@#{self.shopify_domain}/admin"
     #5b6c8beffd764fc95d6bcde8459c0b2a
-    "https://5b6c8beffd764fc95d6bcde8459c0b2a:#{self.shopify_token}@#{self.shopify_domain}/admin"
+    "https://5b6c8beffd764fc95d6bcde8459c0b2a:68348e769f6cb3c191854d45d947ab81@projecth.myshopify.com/admin"
   end
 end
