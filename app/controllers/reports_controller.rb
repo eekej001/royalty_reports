f2347732b6876ad3d471d61510bafe0fbacd413d
Ben <eekej001@gmail.com>
2017-11-20 15:36:50 -0800

    added paypal payout api code

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,13 +1,16 @@
-class ReportsController < ShopifyApp::AuthenticatedController
-
- def index
-    @artists = Artist.all
-
       require 'paypal-sdk-rest'
       require 'securerandom'
       #require './config/runner.rb'
       include PayPal::SDK::Rest
       include PayPal::SDK::Core::Logging
+
+
+class ReportsController < ShopifyApp::AuthenticatedController
+
+ def index
+    @artists = Artist.all
+
+
 	  PayPal::SDK.configure(
 	  :mode => "sandbox", # "sandbox" or "live"
 	  :client_id => ENV["CLIENT_ID"],
